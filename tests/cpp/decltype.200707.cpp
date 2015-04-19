21b168c87ecd0ae547032a5c507533b5943b3045
K-ballo <k@fusionfenix.com>
2015-04-19 13:47:27 -0300

    Fixed missing decltype test

--- tests/cpp/decltype.200707.cpp
+++ tests/cpp/decltype.200707.cpp
@@ -5,10 +5,20 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-void test_static_assert() {
-  static_assert(true, "");
+template <typename T> struct is_lvref { enum { value = false }; };
+template <typename T> struct is_lvref<T&> { enum { value = true }; };
+
+template <typename T> struct is_rvref { enum { value = false }; };
+template <typename T> struct is_rvref<T&&> { enum { value = true }; };
+
+void test_decltype() {
+  int lv = 0;
+  decltype((lv)) ilv = (lv);
+  int check_lvref[is_lvref<decltype(ilv)>::value ? 1 : -1];
+  decltype(static_cast<int&&>(lv)) irv = static_cast<int&&>(lv);
+  int check_rvref[is_rvref<decltype(irv)>::value ? 1 : -1];
 }
 
 int main() {
-  test_static_assert();
+  test_decltype();
 }
